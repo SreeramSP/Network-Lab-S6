1ebedd95b1716a71f19ed9b76cb9077bc1545a45
Sreeram SP <45159343+SreeramSP@users.noreply.github.com>
2021-04-06 22:25:51 +0530

    Add files via upload

---
+++ exp3fork1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+        int x;
+        x =  fork();
+        if(x==0)
+        {
+                printf("C1 PID = %d Parent = %d\n ", getpid(), getppid());
+                x = fork();
+                if(x==0)
+                {
+                        printf("C3 PID = %d Parent = %d\n", getpid(), getppid());
+                }
+                else if(x>0)
+                {
+                        x = fork();
+                        if(x==0)
+                        {
+                                printf("C4 PID = %d Parent = %d\n", getpid() , getppid());
+                        }
+                }
+        }
+         else if(x>0)
+        {
+                printf("P PID = %d\n ", getpid());
+                x =fork();
+                if(x==0)
+                {
+                        printf("C2 PID = %d Parent = %d\n", getpid(), getppid());
+                }
+        }
+return 0;
+}
+
+  
