bae090250aa3a115acc181a6afed77e976b06ead
Sreeram SP <45159343+SreeramSP@users.noreply.github.com>
2021-07-22 23:40:16 +0530

    Add files via upload

---
+++ labexam.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<stdlib.h>
+void main(){
+    int key=ftok("shmfile",65);
+    int shmid=shmget(key,1024,0666|IPC_CREAT);
+    int *shmptr=shmat(shmid,NULL,0);
+    int num;
+    int f=fork();
+    if(f==1){
+        printf("Fork creation failed\n");
+        exit(1);
+    }
+    else if(f>0){ //Parent process
+        wait();
+        int rev = 0, remainder;
+        int val=*shmptr;
+        while (val != 0) {
+        remainder = val % 10;
+        rev = rev * 10 + remainder;
+        val /= 10;
+    }
+        printf("Reversed number = %d", rev);
+        
+    }
+    else{//Child process
+        printf("Enter a number= ");
+        scanf("%d",&num);
+        *shmptr=num;
+    }
+    shmdt(shmptr);
+    shmctl(shmid,IPC_RMID,NULL);
+}
